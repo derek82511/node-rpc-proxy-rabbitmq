2aa42534559df3c2059b58aa223bfe3e028f9831
ChangCheng-Yi <derek82511@gmail.com>
2019-07-10 18:42:06 +0800

    log update

--- services/rpc/rpc-client.js
+++ services/rpc/rpc-client.js
@@ -49,7 +49,7 @@ RPCClient.prototype.start = function () {
             })
 
             this.mq.connection.on('error', err => {
-                this.logger.error(`${this.description} error : ${err}`)
+                this.logger.error(`${this.description} error = ${err}`)
                 this.isReady = false
             })
 
@@ -62,11 +62,15 @@ RPCClient.prototype.start = function () {
                 .then(q => {
                     this.rpcQueues.replyQueueName = q.queue
 
-                    this.logger.info(`${this.description} replyQueueName : ${this.rpcQueues.replyQueueName}`)
+                    this.logger.info(`${this.description} replyQueueName = ${this.rpcQueues.replyQueueName}`)
 
                     this.mq.channel.consume(this.rpcQueues.replyQueueName, msg => {
+                        let message = msg.content.toString('utf8');
+
+                        this.logger.info(`${this.description} Channel receive reply. replyQueueName = ${this.rpcQueues.replyQueueName}, correlationId = ${msg.properties.correlationId}, message = ${message}`)
+
                         if (this.requestDefers[msg.properties.correlationId]) {
-                            this.logger.info(`${this.description} Channel receive reply. correlationId = ${msg.properties.correlationId}`)
+                            this.logger.info(`${this.description} Channel reply correlationId map success. replyQueueName = ${this.rpcQueues.replyQueueName}, correlationId = ${msg.properties.correlationId}`)
 
                             let content = JSON.parse(msg.content.toString('utf8'))
 
@@ -79,9 +83,9 @@ RPCClient.prototype.start = function () {
                     return Promise.resolve()
                 })
                 .then(() => this.isReady = true)
-                .catch(err => this.logger.error(`${this.description} channel err ${err}`))
+                .catch(err => this.logger.error(`${this.description} channel error = ${err}`))
         }).catch(err => {
-            this.logger.error(`${this.description} connection err ${err}`)
+            this.logger.error(`${this.description} connection error = ${err}`)
 
             setTimeout(this.start.bind(this), retryTimeout)
         })
@@ -111,20 +115,22 @@ RPCClient.prototype.call = function call(body) {
 
     this.requestDefers[correlationId] = requestDefer
 
-    this.logger.info(`${this.description} Channel send request ... correlationId = ${correlationId}`)
+    let message = JSON.stringify(content);
+
+    this.logger.info(`${this.description} Channel send request ... requestQueueName = ${this.rpcQueues.requestQueueName}, correlationId = ${correlationId}, message = ${message}`)
 
     this.mq.channel.sendToQueue(this.rpcQueues.requestQueueName,
-        Buffer.from(JSON.stringify(content), 'utf8'), {
+        Buffer.from(message, 'utf8'), {
             persistent: true,
             correlationId: correlationId,
             replyTo: this.rpcQueues.replyQueueName
         }, err => {
             if (err !== null) {
-                this.logger.error(`${this.description} Channel send request failed. correlationId = ${correlationId}, err = ${err}`)
+                this.logger.error(`${this.description} Channel send request failed. requestQueueName = ${this.rpcQueues.requestQueueName}, correlationId = ${correlationId}, error = ${err}`)
 
                 requestDefer.reject(`${this.description} unavailable. ${err}`)
             } else {
-                this.logger.info(`${this.description} Channel send request success. correlationId = ${correlationId}`)
+                this.logger.info(`${this.description} Channel send request success. requestQueueName = ${this.rpcQueues.requestQueueName}, correlationId = ${correlationId}`)
             }
         })
 
